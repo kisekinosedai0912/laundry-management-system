7dc00f0a8ae1649dad1aeecb730d6be823744813
jasper <kisekinosedai0912@gmail.com>
2025-12-12 08:53:09 +0800

    modified server.js for production

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import bookingRoutes from './routes/booking.routes.js';
 
 const app = express();
 const PORT = 5000;
+const NODE_ENV = 'production';
 
 app.use(cors({
     origin: 'https://laundry-management-system-32ft.onrender.com', 
@@ -46,7 +47,7 @@ app.use("/api/bookings", bookingRoutes);
 */
 const __dirname = path.resolve();
 
-if (process.env?.NODE_ENV === 'production') {
+if (NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
     app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
